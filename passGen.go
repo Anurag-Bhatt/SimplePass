b3a9174b9464e0f484a6d5cb5e31d3f7d1c69c49
Anurag <anuragbhatt865@gmail.com>
2023-05-19 06:38:01 +0530

    Initial Commit

---
+++ passGen.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+
+	rand.Seed(time.Now().UnixNano())
+
+	// passGen -size
+	passSize, err := strconv.Atoi(os.Args[1])
+
+	fmt.Println(passSize)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	allowedChars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-="
+
+	newPassword := ""
+
+	for i := 0; i < passSize; i++ {
+		// Generate a random ASCII character
+		randomChar := allowedChars[rand.Intn(62)]
+
+		// Append the random character to the new password string
+		newPassword += string(randomChar)
+	}
+
+	fmt.Println(newPassword)
+}
