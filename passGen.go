e05576054eb994e6d1a224c1f627be3556bb26cb
Anurag <anuragbhatt865@gmail.com>
2023-05-31 00:07:08 +0530

    Added warning on pass size less than 8 Added functionality for complex passwords Resolved some bugs

--- passGen.go
+++ passGen.go
@@ -16,8 +16,6 @@ func main() {
 	// passGen -size
 	passSize, err := strconv.Atoi(os.Args[1])
 
-	fmt.Println(passSize)
-
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -25,11 +23,37 @@ func main() {
 
 	allowedChars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-="
 
+	allowedCapitalChars := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	allowedSmallChars := "abcdefghijklmnopqrstuvwxyz"
+	allowedDigits := "0123456789"
+	allowedSymbols := "!@#$%^&*()_+-="
+
 	newPassword := ""
 
-	for i := 0; i < passSize; i++ {
+	randomChar := allowedCapitalChars[rand.Intn(25)]
+	newPassword += string(randomChar)
+	// fmt.Println(newPassword)
+
+	randomChar = allowedSmallChars[rand.Intn(25)]
+	newPassword += string(randomChar)
+	// fmt.Println(newPassword)
+
+	randomChar = allowedDigits[rand.Intn(10)]
+	newPassword += string(randomChar)
+	// fmt.Println(newPassword)
+
+	randomChar = allowedSymbols[rand.Intn(13)]
+	newPassword += string(randomChar)
+	// fmt.Println(newPassword)
+
+	if passSize < 8 {
+		passSize = 8
+		println("WARNING: The password must be a minimum of 8 characters in length.")
+	}
+
+	for i := 0; i < passSize-4; i++ {
 		// Generate a random ASCII character
-		randomChar := allowedChars[rand.Intn(62)]
+		randomChar = allowedChars[rand.Intn(62)]
 
 		// Append the random character to the new password string
 		newPassword += string(randomChar)
